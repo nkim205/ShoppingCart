0d2108bc13b7fdd972aa736879430b97cd151218
Nathan D. Kim <kimnathan0324@gmail.com>
2025-05-19 19:51:31 -0400

    final

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,9 +3,9 @@ import NavButton from "./NavButton";
 
 const NavBar = ({numitem}) => {
     return (      
-        <div className="bg-gray-400 flex flex-row justify-between">
+        <div className="bg-gray-400 flex flex-row justify-between items-center">
             <NavButton path="./images/home.png" alt="homebtn" desc="/"/>
-            <p>{numitem}</p>
+            <p className="text-center text-3xl">{numitem}</p>
             <NavButton path="./images/cart.png" alt="carbtn" desc="/cart"/>
         </div>
     )
